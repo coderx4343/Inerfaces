d8ed7d645eff825107b0b46faf3f9120137e6b35
coderx4343 <kannangk4343@gmail.com>
2022-08-18 23:27:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-9gf8uh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9gf8uh)
---
+++ index.ts
@@ -0,0 +1,42 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+interface IPerson { 
+  firstName:string, 
+  lastName:string, 
+  sayHi: ()=>void,
+  address:string,
+  ph_no:number 
+} 
+
+var customer:IPerson = { 
+  firstName:"Kannan",
+  lastName:"G", 
+  sayHi: ():void =>{console.log("Hi there")},
+  address:"Sivaksi - 626 189",
+  ph_no: 983893787
+} 
+
+console.log("Customer Object ") 
+console.log(customer);
+console.table(customer);
+console.log(customer.firstName) 
+console.log(customer.lastName) 
+console.log(customer.sayHi())  
+
+var employee:IPerson = { 
+  firstName:"Kasinath",
+  lastName:"S", 
+  sayHi: ():void =>{console.log("Hello!!!")},
+  address:"Thiruthangal - 626 123",
+  ph_no:8987677678 
+} 
+ 
+console.log("Employee  Object ");
+console.log(employee);
+console.table(employee);
+console.log(employee.firstName);
+console.log(employee.lastName);
+console.log(customer.sayHi());
